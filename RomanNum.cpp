52a62fe6072f1926d77bd56a398040eecdeb29ff
zaratejavier <javierzaratemata@mail.weber.edu>
2017-10-26 13:12:10 -0600

    Initial commit

---
+++ RomanNum.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Javier Zarate on 10/26/17.
+//
+
+#include "RomanNum.h"
+
+using namespace std;
---
+++ RomanNum.h
@@ -0,0 +1,19 @@
+//
+// Created by Javier Zarate on 10/26/17.
+//
+
+#ifndef ROMANTEST_ROMANNUM_H
+
+#include <iostream>
+
+#define ROMANTEST_ROMANNUM_H
+
+
+using namespace std;
+
+class RomanNum {
+
+};
+
+
+#endif //ROMANTEST_ROMANNUM_H
